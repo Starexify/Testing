73008f7823c1c59b9b49cc74395e98c986aad667
Starexify <nightbear843@yahoo.com>
2025-01-19 11:36:04 +0100

    Test #1

---
+++ src/client/java/net/nova/testing/TestingClient.java
@@ -0,0 +1,10 @@
+package net.nova.testing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/nova/testing/Testing.java
@@ -0,0 +1,16 @@
+package net.nova.testing;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Testing implements ModInitializer {
+	public static final String MOD_ID = "testing";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
