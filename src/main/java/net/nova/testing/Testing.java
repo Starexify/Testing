598fb8be82a92dbbd3c19dbe813bc0c85794964c
Starexify <nightbear843@yahoo.com>
2025-01-19 17:38:19 +0100

    1.21.1 Neo

--- src/main/java/net/nova/testing/Testing.java
+++ src/main/java/net/nova/testing/Testing.java
@@ -1,37 +1,20 @@
 package net.nova.testing;
 
-import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ShieldItem;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
 
-import java.util.function.Function;
+import static net.nova.testing.Testing.MODID;
 
-public class Testing implements ModInitializer {
+@Mod(MODID)
+public class Testing {
     public static final String MODID = "testing";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-    @Override
-    public void onInitialize() {
-        Item TEMPLATE_SHIELD_1 = registerItem("template_shield_1", ShieldItem::new);
-        Item TEMPLATE_SHIELD_2 = registerItem("template_shield_2", TemplateShieldItem::new);
+    public Testing(IEventBus bus) {
+
     }
 
-    public static Item registerItem(String name, Function<Item.Settings, Item> factory) {
-        return register(name, factory, new Item.Settings());
-    }
-
-    public static Item register(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
-        return Registry.register(Registries.ITEM, RegistryKey.of(RegistryKeys.ITEM, rl(name)), factory.apply(settings.registryKey(RegistryKey.of(RegistryKeys.ITEM, rl(name)))));
-    }
-
-    public static Identifier rl(String path) {
-        return Identifier.of(MODID, path);
+    public static ResourceLocation rl(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 }
