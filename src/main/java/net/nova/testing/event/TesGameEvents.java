d53b52cd907761ac750602191ede7eb45851407c
Starexify <nightbear843@yahoo.com>
2025-01-24 17:33:44 +0100

    1.21.1 Neo

---
+++ src/main/java/net/nova/testing/event/TesGameEvents.java
@@ -0,0 +1,9 @@
+package net.nova.testing.event;
+
+import net.neoforged.fml.common.EventBusSubscriber;
+
+import static net.nova.testing.Testing.MODID;
+
+@EventBusSubscriber(modid = MODID)
+public class TesGameEvents {
+}
