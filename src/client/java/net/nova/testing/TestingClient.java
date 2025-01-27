9592098cf73643f79015b5826933ae0c7dbe4078
Starexify <nightbear843@yahoo.com>
2025-01-27 17:48:00 +0100

    1.21.4 Fabric

--- src/client/java/net/nova/testing/TestingClient.java
+++ src/client/java/net/nova/testing/TestingClient.java
@@ -1,11 +1,9 @@
 package net.nova.testing;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.texture.ResourceTexture;
 
 public class TestingClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-
 	}
 }
