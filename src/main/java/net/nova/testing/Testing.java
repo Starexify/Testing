dd62e1e9b0a4f705784687fff700e5a48ecbaf54
Starexify <nightbear843@yahoo.com>
2025-01-28 17:28:17 +0100

    1.21.4 Fabric

--- src/main/java/net/nova/testing/Testing.java
+++ src/main/java/net/nova/testing/Testing.java
@@ -1,17 +1,9 @@
 package net.nova.testing;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ShieldItem;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.function.Function;
 
 public class Testing implements ModInitializer {
     public static final String MODID = "testing";
@@ -19,16 +11,6 @@ public class Testing implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Item TEMPLATE_SHIELD_1 = registerItem("template_shield_1", ShieldItem::new);
-
-    }
-
-    public static Item registerItem(String name, Function<Item.Settings, Item> factory) {
-        return register(name, factory, new Item.Settings());
-    }
-
-    public static Item register(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
-        return Registry.register(Registries.ITEM, RegistryKey.of(RegistryKeys.ITEM, rl(name)), factory.apply(settings.registryKey(RegistryKey.of(RegistryKeys.ITEM, rl(name)))));
     }
 
     public static Identifier rl(String path) {
