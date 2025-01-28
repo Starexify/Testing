2594b33ec467e0c13df537def07cb203422a7730
Starexify <nightbear843@yahoo.com>
2025-01-28 17:51:56 +0100

    1.21.4 Fabric

---
+++ src/main/java/net/nova/block/TestBlock.java
@@ -0,0 +1,21 @@
+package net.nova.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class TestBlock extends Block {
+    public TestBlock(Settings settings) {
+        super(settings);
+    }
+
+    /// This function is called when an entity interacts with the block (such as right-clicking it)
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        return super.onUse(state, world, pos, player, hit);
+    }
+}
