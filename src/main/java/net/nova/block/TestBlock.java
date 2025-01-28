ec66ac310b5bccda91ac8b9f10bdf545611561a4
Starexify <nightbear843@yahoo.com>
2025-01-28 18:04:42 +0100

    Some random documentation

--- src/main/java/net/nova/block/TestBlock.java
+++ src/main/java/net/nova/block/TestBlock.java
@@ -3,7 +3,9 @@ package net.nova.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -17,5 +19,12 @@ public class TestBlock extends Block {
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         return super.onUse(state, world, pos, player, hit);
+    }
+
+    /// This function is called when an entity interacts with the block with an Item in hand (such as right-clicking it)
+    /// The only diff from #onUse() being that it returns an ItemStack and Hand
+    @Override
+    protected ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        return super.onUseWithItem(stack, state, world, pos, player, hand, hit);
     }
 }
