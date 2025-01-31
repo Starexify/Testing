db2cd8feacf27fdb9cea62497c14225ba7c17451
Starexify <nightbear843@yahoo.com>
2025-01-31 18:32:54 +0100

    Data Gen

---
+++ src/main/java/net/nova/testing/data/DataGenerators.java
@@ -0,0 +1,15 @@
+package net.nova.testing.data;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import static net.nova.testing.Testing.MODID;
+
+@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent.Client event) {
+
+    }
+}
