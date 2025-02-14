bcf77743194274397b9828dce74bfe5a3386f876
Starexify <nightbear843@yahoo.com>
2025-02-14 17:07:05 +0100

    1.21.4 NeoForge

--- src/main/java/net/nova/testing/event/ModEvents.java
+++ src/main/java/net/nova/testing/event/ModEvents.java
@@ -2,18 +2,26 @@ package net.nova.testing.event;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.EntityAttributeModificationEvent;
 
 import static net.nova.testing.Testing.MODID;
 
 @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ModEvents {
 
-    // Lets you add new Attributes to EntitiesF
+    // Lets you create Attributes for Entities
     @SubscribeEvent
     public static void addAttribute(EntityAttributeCreationEvent event) {
         event.put(EntityType.PLAYER, new AttributeSupplier.Builder().build());
+    }
+
+    // Lets you modify the Attributes to Entities
+    @SubscribeEvent
+    public static void addAttribute(EntityAttributeModificationEvent event) {
+        event.add(EntityType.PLAYER, Attributes.ATTACK_DAMAGE, 1);
     }
 }
