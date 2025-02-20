57b9489a23422a6b40cd66dad557b10afb2cfabb
Starexify <nightbear843@yahoo.com>
2025-02-20 18:35:15 +0100

    1.21.4 NeoForge

--- src/main/java/net/nova/testing/event/Events.java
+++ src/main/java/net/nova/testing/event/Events.java
@@ -3,6 +3,7 @@ package net.nova.testing.event;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.BlockEntityTypeAddBlocksEvent;
+import net.neoforged.neoforge.event.entity.EntityEvent;
 
 import static net.nova.testing.Testing.MODID;
 
@@ -13,5 +14,10 @@ public class Events {
     @SubscribeEvent
     public static void addBlockToBE(BlockEntityTypeAddBlocksEvent event) {
         // event.modify(BlockEntityType.HANGING_SIGN, Blocks.LIGHT_BLUE_WOOL);
+    }
+
+    // Lets you fire logic on Entity constructor
+    public static void entityConstruct(EntityEvent.EntityConstructing event) {
+
     }
 }
