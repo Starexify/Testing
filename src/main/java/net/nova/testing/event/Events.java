2fc55b8877b511444c87ab8974585c50503bc227
ImVeryBad <nightbear843@yahoo.com>
2025-04-12 14:11:49 +0200

    1.21.4 NeoForge

--- src/main/java/net/nova/testing/event/Events.java
+++ src/main/java/net/nova/testing/event/Events.java
@@ -4,11 +4,17 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.BlockEntityTypeAddBlocksEvent;
 import net.neoforged.neoforge.event.entity.EntityEvent;
+import net.neoforged.neoforge.event.level.BlockDropsEvent;
 
 import static net.nova.testing.Testing.MODID;
 
 @EventBusSubscriber(modid = MODID)
 public class Events {
+
+    // Lets you fire logic when a Block drops it's contents
+    @SubscribeEvent
+    public static void onDropEvent(BlockDropsEvent event) {
+    }
 
     // Lets you add new Blocks to Block Entities
     @SubscribeEvent
@@ -17,6 +23,7 @@ public class Events {
     }
 
     // Lets you fire logic on Entity constructor
+    @SubscribeEvent
     public static void entityConstruct(EntityEvent.EntityConstructing event) {
 
     }
