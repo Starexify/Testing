a4345d7514724b4d2d12db3f2a9bf10988172d89
ImVeryBad <nightbear843@yahoo.com>
2025-04-17 18:53:42 +0200

    1.7.10 Legacy Fabric

--- src/main/java/net/nova/testing/Testing.java
+++ src/main/java/net/nova/testing/Testing.java
@@ -1,20 +1,12 @@
 package net.nova.testing;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.resources.ResourceLocation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Testing implements ModInitializer {
     public static final String MODID = "testing";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     @Override
     public void onInitialize() {
 
-    }
-
-    public static ResourceLocation rl(String path) {
-        return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 }
